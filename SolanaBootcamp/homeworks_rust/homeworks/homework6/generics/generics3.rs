a830a311f037f84b9b9c865629531cef14e704b3
Dominique Luna <dluna132@gmail.com>
2024-04-03 10:54:01 -0400

    progress

--- SolanaBootcamp/homeworks_rust/homeworks/homework6/generics/generics3.rs
+++ SolanaBootcamp/homeworks_rust/homeworks/homework6/generics/generics3.rs
@@ -9,19 +9,35 @@
 // to show that your changes allow alphabetical grades.
 
 // Execute 'rustlings hint generics3' for hints!
+use std::fmt;
+use std::fmt::Display;
 
-// I AM NOT DONE
+pub enum Grade {
+    Numeric(f32),
+    Alphabetic(String),
+}
+
+impl fmt::Display for Grade {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Grade::Numeric(grade) => write!(f, "{}", grade),
+            Grade::Alphabetic(grade) => write!(f, "{}", grade),
+        }
+    }
+}
 
 pub struct ReportCard {
-    pub grade: f32,
+    pub grade: Grade,
     pub student_name: String,
     pub student_age: u8,
 }
 
 impl ReportCard {
     pub fn print(&self) -> String {
-        format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+        format!(
+            "{} ({}) - achieved a grade of {}",
+            &self.student_name, &self.student_age, &self.grade,
+        )
     }
 }
 
@@ -32,7 +48,7 @@ mod tests {
     #[test]
     fn generate_numeric_report_card() {
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: Grade::Numeric(2.1),
             student_name: "Tom Wriggle".to_string(),
             student_age: 12,
         };
@@ -44,9 +60,8 @@ mod tests {
 
     #[test]
     fn generate_alphabetic_report_card() {
-        // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: Grade::Alphabetic(String::from("A+")),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
