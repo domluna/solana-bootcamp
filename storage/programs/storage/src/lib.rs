3f0f935f4b6871d6e56f733e7191964a1466b768
Dominique Luna <dluna132@gmail.com>
2024-04-18 18:33:10 -0400

    shellfish:

--- storage/programs/storage/src/lib.rs
+++ storage/programs/storage/src/lib.rs
@@ -6,9 +6,20 @@ declare_id!("4wcTXvHy5d6vytHCcMiokMqYdj26QY2o5bB6M4gA5uQ4");
 pub mod storage {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>, balance: u64) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let balance = 100;
         ctx.accounts.storage_account.balance = balance;
-        msg!("Changed data to: {}!", balance);
+        msg!("Initialized data to: {}!", balance);
+        Ok(())
+    }
+
+    pub fn update_balance(ctx: Context<UpdateBalance>) -> Result<()> {
+        let data: &mut Account<StorageData> = &mut ctx.accounts.storage_account;          
+        if data.balance >= 1000 {
+            return Err(ErrorCode::BalanceLimitReached.into());
+        }
+        data.balance += 100;
+        msg!("Changed data to: {}!", data.balance);
         Ok(())
     }
 }
@@ -28,7 +39,22 @@ pub struct Initialize<'info> {
     pub system_program: Program<'info, System>,
 }
 
+#[derive(Accounts)]
+pub struct UpdateBalance<'info> {
+    #[account(mut, 
+        constraint = storage_account.balance > 0,
+    )]       
+    pub storage_account: Account<'info, StorageData>,
+}
+
 #[account]
 pub struct StorageData {
     balance: u64,
+}
+
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Balance limit of 1000 reached or exceeded.")]
+    BalanceLimitReached,
 }
--- storage/tests/storage.ts
+++ storage/tests/storage.ts
@@ -1,8 +1,8 @@
-import * as anchor from '@coral-xyz/anchor';
-import { Program } from '@coral-xyz/anchor';
-import { Storage } from '../target/types/storage';
-import { expect } from 'chai';
-import { before } from 'mocha';
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { Storage } from "../target/types/storage";
+import { expect } from "chai";
+import { before } from "mocha";
 
 // Configure the client to use the local cluster.
 const provider = anchor.AnchorProvider.env();
@@ -12,35 +12,78 @@ const program = anchor.workspace.Storage as Program<Storage>;
 const systemProgram = anchor.web3.SystemProgram.programId;
 let storageDataPDA;
 
-describe('storage', async () => {
-	it('Intialize', async () => {
-		try {
-			const data = new anchor.BN(100);
-			[storageDataPDA] = await anchor.web3.PublicKey.findProgramAddressSync(
-				[provider.publicKey.toBuffer()],
-				program.programId
-			);
-			const tx = await program.methods
-				.initialize(data)
-				.accounts({
-					storageAccount: storageDataPDA,
-					systemProgram: systemProgram,
-				})
-				.signers([])
-				.rpc();
-			console.log('🚀 Intialization transaction:', tx);
-		} catch (error) {
-			console.error(
-				'Check if pda is already in use after initializing account',
-				error.logs[3]
-			);
-		}
-	});
-	it('Account data is initialized to 100', async () => {
-		// fetch data for pda
-		const data = await program.account.storageData.fetch(storageDataPDA);
-		// convert BN to decimal
-		const value = parseInt(data.balance.toString('hex'), 16);
-		expect(value).equal(100, 'Value is not 100');
-	});
+describe("storage", async () => {
+  it("Intialize", async () => {
+    try {
+      [storageDataPDA] = await anchor.web3.PublicKey.findProgramAddressSync(
+        [provider.publicKey.toBuffer()],
+        program.programId
+      );
+      const tx = await program.methods
+        .initialize()
+        .accounts({
+          storageAccount: storageDataPDA,
+          systemProgram: systemProgram,
+        })
+        .signers([])
+        .rpc();
+      console.log("🚀 Intialization transaction:", tx);
+    } catch (error) {
+      console.error(
+        "Check if pda is already in use after initializing account",
+        error.logs[3]
+      );
+    }
+  });
+
+  it("Account data is initialized to 100", async () => {
+    // fetch data for pda
+    const data = await program.account.storageData.fetch(storageDataPDA);
+    // convert BN to decimal
+    const value = parseInt(data.balance.toString("hex"), 16);
+    expect(value).equal(100, "Value is not 100");
+  });
+
+  it("Increment balance up to 1000", async () => {
+    for (let i = 0; i < 9; i++) {
+      let data = await program.account.storageData.fetch(storageDataPDA);
+      const oldValue = parseInt(data.balance.toString("hex"), 16);
+      const tx = await program.methods
+        .updateBalance()
+        .accounts({
+          storageAccount: storageDataPDA,
+        })
+        .signers([])
+        .rpc();
+
+      console.log("update transaction", tx);
+
+      data = await program.account.storageData.fetch(storageDataPDA);
+      const newValue = parseInt(data.balance.toString("hex"), 16);
+      expect(newValue).equal(oldValue + 100, `Value is not ${oldValue + 100}`);
+    }
+    // check for 1000 value
+    //
+
+    const data = await program.account.storageData.fetch(storageDataPDA);
+    const newValue = parseInt(data.balance.toString("hex"), 16);
+    expect(newValue).equal(1000, `Value is not 1000`);
+  });
+
+  it("Throw error when trying to update balance at 1000", async () => {
+    try {
+      await program.methods
+        .updateBalance()
+        .accounts({
+          storageAccount: storageDataPDA,
+        })
+        .signers([])
+        .rpc();
+      expect.fail("Expected an error to be thrown");
+    } catch (error) {
+      expect(error.error.errorMessage).to.equal(
+        "Balance limit of 1000 reached or exceeded."
+      );
+    }
+  });
 });
